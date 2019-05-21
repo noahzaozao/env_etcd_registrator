d3eefb35d60e8d7860d70cad264d4970079f7eec
wzy(Noah Wang) <234082230@qq.com>
2019-05-21 13:38:30 +0800

    update stack

--- swarm_deploy.sh
+++ swarm_deploy.sh
@@ -1,4 +1,4 @@
 export ETCD_IP=192.168.3.170
 export ETCD_NODE1_IP=192.168.3.21
 export ETCD_NODE2_IP=192.168.3.33
-docker stack deploy -c docker-stack.yml etcd_registrator
+docker docker-compose -f docker-compose-swarm.yml up -d etcd
